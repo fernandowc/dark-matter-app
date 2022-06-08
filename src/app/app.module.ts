180e746466b2a03d1eaa93ce8787209d018f3f39
Luis Vidal <lvidaldev@gmail.com>
2022-06-08 02:11:07 -0500

    se crea proyecto y algunas cosas mas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { TestComponent } from './pages/test/test.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotfoundComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    iduser: number;
+    firstname: string;
+    secondname: string;
+    lastname: string;
+    surname: string;
+    urlfoto: string;
+}
